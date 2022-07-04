7f4c8ced21f6f2134a05d8150e2db493cf969d6d
nelio1977 <69060111+nelio1977@users.noreply.github.com>
2022-07-04 19:18:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+const inputEl= document.getElementById("inputEl")
+const btnEl= document.getElementById("btnEl")
+const tabEl= document.getElementById("tabEl")
+const ulEl= document.getElementById("ulEl")
+const delBtn= document.getElementById("del")
+let myLeads= []
+
+const leadsFromStorage= JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromStorage) {
+    myLeads= leadsFromStorage
+    render()
+}
+
+function render() {
+    let list= "";
+   myLeads.forEach(leads => {
+        list += `<li><a target="_blank" href="${leads}">${leads}</a></li>`;
+   }) 
+   ulEl.innerHTML= list;
+}
+  
+delBtn.addEventListener("click", ()=> {
+    localStorage.clear()
+     myLeads= [];
+    render()
+
+})
+
+btnEl.addEventListener("click", ()=> {
+    if(inputEl.value) {
+    myLeads.push(inputEl.value)
+    inputEl.value= ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render()
+    }
+})   
+   
+tabEl.addEventListener("click", ()=> {
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url)
+    
+
+  });
+})
+
+
+
+
+
